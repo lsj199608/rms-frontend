7d0f2ce04ad9702180778e6e0527c839051f54c3
lsj <lsj199608@naver.com>
2026-03-04 23:50:01 +0900

    axios env 겹침 해소위한 axios 수정하여 커밋

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const API_BASE_URL: string =
-    import.meta.env.VITE_API_URL?.trim() || 'http://localhost:9090';
+const envApiUrl = import.meta.env.VITE_API_URL?.trim() || 'http://localhost:9090';
+
+export const API_BASE_URL: string = `${window.location.hostname}`.includes("rms")
+      ? `${window.location.protocol}//${window.location.hostname}` : envApiUrl;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
